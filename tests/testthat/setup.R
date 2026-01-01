a5b264625b083509da91763696e4a266f097f082
SInginc <singing.hgiua@gmail.com>
2026-01-01 13:32:22 +0800

    chore: set `R_USER_CACHE_DIR` to tempdir() for R CMD check

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,1 @@
-if (identical(Sys.getenv("_R_CMD_CHECK"), "true")) {
-  # Set R_USER_CACHE_DIR to a temporary path during R CMD check
-  Sys.setenv(R_USER_CACHE_DIR = tempdir())
-}
+Sys.setenv(R_USER_CACHE_DIR = tempdir())
